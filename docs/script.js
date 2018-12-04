5223442659899e0b99dca2f8875e9191eccb1bf5
Ryan Yang <javathon@gmail.com>
2018-12-04 14:39:35 -0800

    better date

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 function load(){
     let divName = 'timer-main';
-    let setDate = new Date('June 1, 2020');
+    let setDate = new Date('June 1, 2021');
 
     let timer = new cdtimer(divName, setDate);
 }
