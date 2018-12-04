d31f2ed44d382df3a0432cef5fe48783b8aafd23
Ryan Yang <javathon@gmail.com>
2018-12-04 14:18:47 -0800

    fixing up file sources

---
+++ docs/src/countdown-timer.js
@@ -0,0 +1,55 @@
+'use strict';
+
+class cdtimer{
+    constructor(div, date){
+        this.div = div;
+        this.finish = new Date(date);
+        this.init();
+        /**
+         * Using arrow function here bypasses weird bug where you have to pass 'this' parameter in setInterval
+         * 
+         * setInterval( this.update().bind(this), 200);
+         */
+        setInterval(() => this.update(), 1000);
+    }
+    init(){
+        this.current = new Date();
+        this.calculate();
+        this.check();
+        document.getElementById(this.div).insertAdjacentHTML('beforeend', `
+                <div class="ctd-block" id="ctd-year">${this.year}</div>
+                <div class="ctd-block" id="ctd-month">${this.month}</div>
+                <div class="ctd-block" id="ctd-day">${this.day}</div>
+                <div class="ctd-block" id="ctd-hour">${this.hour}</div>
+                <div class="ctd-block" id="ctd-minute">${this.minute}</div>
+                <div class="ctd-block" id="ctd-second">${this.second}</div>
+            `);
+            console.log('done');
+    }
+    update(){
+        this.current = new Date();
+        this.calculate();
+        this.check();
+        this.display();
+    }
+    calculate(){
+        this.diff = this.finish.getTime() - this.current.getTime();
+        this.diff = (this.diff/1000) >> 0;
+    }
+    check(){
+        this.year = this.diff/31557600 >> 0;
+        this.month = (this.diff % 31557600) / 2592000 >> 0;
+        this.day = (this.diff % 2592000) / 86400 >> 0;
+        this.hour = (this.diff % 86400) / 3600 >> 0;
+        this.minute = (this.diff % 3600) / 60 >> 0;
+        this.second = this.diff % 60;
+    }
+    display(){
+        document.getElementById('ctd-year').innerHTML = `${this.year}`;
+        document.getElementById('ctd-month').innerHTML = `${this.month}`;
+        document.getElementById('ctd-day').innerHTML = `${this.day}`;
+        document.getElementById('ctd-hour').innerHTML = `${this.hour}`;
+        document.getElementById('ctd-minute').innerHTML = `${this.minute}`;
+        document.getElementById('ctd-second').innerHTML = `${this.second}`;
+    }
+}
